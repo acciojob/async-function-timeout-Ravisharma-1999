6644e0012a8d0205b711b957147e719566230a0d
Ravisharma-1999 <ravi829144@gmail.com>
2024-10-18 19:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your JS code here. If required.
-document.getElementById('btn').addEventListener('click',async function (){
+document.getElementById('btn').addEventListener('click', async function (){
 	const textInput = document.getElementById('text').value;
-		const delayInput = document.getElementById('delay').value;
+    const delayInput = document.getElementById('delay').value;
 	const outputDiv = document.getElementById('output');
-	const delay = parseInt(delayinput, 10);
+	
+	const delay = parseInt(delayInput, 10);
+	
 	outputDiv.textContent = "";
+	
 	const waitForDelay = (ms) => {
+		return new promise((resolve) => {
 		setTimeout(resolve, ms);
 	});
 
