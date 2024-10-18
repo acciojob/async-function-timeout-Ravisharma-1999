cf7bd3c81d030cdbdf235b81430f3eec3d645d2f
Ravisharma-1999 <ravi829144@gmail.com>
2024-10-18 19:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click',async function (){
+	const textInput = document.getElementById('text').value;
+		const textInput = document.getElementById('delay').value;
+	const textInput = document.getElementById('output');
+	const delay = parseInt(delayinput, 10);
+	outputDiv.textContent = "";
+	const waitForDelay = (ms) => {
+		setTimeout(resolve, ms);
+	});
+
+};
+if(textInput && delay){
+	await waitForDelay(delay);
+	outputDiv.textContent = "Please enter valid text and delay.";
+}
+;
